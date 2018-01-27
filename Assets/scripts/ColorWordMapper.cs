e2ebc48b788f685e7e6852034d4d07ef9a82e675
Olle Olsson <ohjolsson@gmail.com>
2018-01-27 04:51:01 +0100

    Refactoring

---
+++ Assets/scripts/ColorWordMapper.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class ColorMapper {
+    private const int perfectScoreBonus = 2;
+    private readonly int wordsPerLine;
+    private List<string[]> words;
+    private List<string[]> colors;
+    private List<int> scorePerLine = new List<int>();
+
+    public ColorMapper (int wordsPerLine) {
+        this.wordsPerLine = wordsPerLine;
+    }
+
+    public int[] GetScores () {
+        return scorePerLine.ToArray();
+    }
+
+    // Returns list where element i contains the colors that correspond to the 
+    // words for player i as sent to ComputeColors
+    public List<string[]> GetColors () {
+        return colors;
+    }
+
+    public void ComputeColors (List<PlayerConnection> players) {
+        words = new List<string[]>(players.Count);
+        colors = new List<string[]>(players.Count);
+
+        foreach (var p in players) {
+            words.Add((string[]) p.Words.Clone());
+            colors.Add(GetInitialColors(wordsPerLine));
+        }
+        for (int i = 0; i < wordsPerLine; i++) {
+            var word = words[0][i];
+            int j = 1;
+            for (; j < players.Count; j++) {
+                if (!word.Equals(words[j][i])) {
+                    break;
+                }
+            }
+            if (j < players.Count)
+                continue;
+            for (int k = 0; k < players.Count; k++) {
+                colors[k][i] = "green";
+                words[k][i] = null;
+            }
+        }
+        var wordDict = new Dictionary<string, uint>();
+        foreach (var line in words) {
+            foreach (var word in line) {
+                if (word == null)
+                    continue;
+                else if (wordDict.ContainsKey(word))
+                    wordDict[word] += 1;
+                else
+                    wordDict[word] = 1;
+            }
+        }
+        foreach (var key in wordDict.Keys.ToList()) {
+            if (wordDict[key] < players.Count)
+                wordDict.Remove(key);
+        }
+        foreach (var key in wordDict.Keys.ToList()) {
+            MarkWordYellow(wordDict, key);
+        }
+    }
+
+    private void MarkWordYellow (Dictionary<string, uint> wordDict, string key) {
+        for (int i = 0; i < wordsPerLine; i++) {
+            for (int j = 0; j < words[i].Length; j++) {
+                if (key.Equals(words[i][j])) {
+                    colors[i][j] = "yellow";
+                    wordDict[key] -= 1;
+                    if (wordDict[key] == 0) {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ComputeScore (int playerIx) {
+        int score = 0;
+        foreach (var c in colors[playerIx]) {
+            if (c == "green")
+                score += 2;
+            else if (c == "yellow")
+                score += 1;
+        }
+        if (PerfectScore(wordsPerLine, score))
+            score += perfectScoreBonus;
+        scorePerLine.Add(score);
+    }
+
+    private static string[] GetInitialColors (int length) {
+        return Enumerable.Repeat("red", length).ToArray();
+    }
+
+    // True if score is such that 2 points per word was awarded
+    private static bool PerfectScore (int wordCount, int score) {
+        return score == wordCount * 2;
+    }
+}
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -10,13 +10,11 @@ public class GameManager : NetworkBehaviour {
     public int linesPerGame;
     public WordSea wordSea;
     public ButtonBar buttonBar;
-    public GameOverScreen gameOverPrefab;
 
     private const int wordsPerLine = 4;
-    private const int perfectScoreBonus = 2;
     private List<LineLog> lineLogs = new List<LineLog>();
-    private List<int> lineScores = new List<int>();
     private List<PlayerConnection> players;
+    private ColorMapper colorWordMapper = new ColorMapper(wordsPerLine);
 
     public override void OnStartServer () {
         players = new List<PlayerConnection>();
@@ -72,12 +70,10 @@ public class GameManager : NetworkBehaviour {
 
     [ClientRpc]
     void RpcAllPlayersReady (string newWordSea) {
-        var wordColors = ComputeWordColors();
-        AddWordsToLineLogs(wordColors);
-
-        var score = ComputeScore(wordColors[0]);  // Could use any player's colors
-        lineScores.Add(score);
-
+        colorWordMapper.ComputeColors(players);
+        colorWordMapper.ComputeScore(0);
+        AddWordsToLineLogs(colorWordMapper.GetColors());
+        
         if (GameOver()) {
             SetUIButtonsInteractable(false);
             ShowGameOverScreen();
@@ -90,127 +86,6 @@ public class GameManager : NetworkBehaviour {
         }
     }
 
-    // Returns list of colors where element i correspond to the words for player i in member players
-    private List<string[]> ComputeWordColors () {
-        List<string[]> wordColors;
-        switch (players.Count) {
-            case 1:
-                wordColors = new List<string[]>() { GetInitialColors(wordsPerLine) };
-                break;
-            case 2:
-                wordColors = Determine2PlayerColors();
-                break;
-            case 3:
-                wordColors = DetermineManyPlayerColors();
-                break;
-            default:
-                throw new ArgumentException();
-        }
-        return wordColors;
-    }
-
-    private List<string[]> Determine2PlayerColors () {
-        string[] p1Words = (string[]) players[0].Words.Clone();
-        string[] p2Words = (string[]) players[1].Words.Clone();
-        string[] p1Colors = GetInitialColors(wordsPerLine);
-        string[] p2Colors = (string[]) p1Colors.Clone();
-
-        for (int i = 0; i < wordsPerLine; i++) {
-            if (p1Words[i].Equals(p2Words[i])) {
-                p1Colors[i] = "green";
-                p2Colors[i] = "green";
-                p1Words[i] = null;
-                p2Words[i] = null;
-            }
-        }
-        for (int i = 0; i < wordsPerLine; i++) {
-            if (p1Words[i] != null) {
-                for (int j = 0; j < wordsPerLine; j++) {
-                    if (p1Words[i].Equals(p2Words[j])) {
-                        p1Colors[i] = "yellow";
-                        p2Colors[j] = "yellow";
-                        p2Words[j] = null;
-                    }
-                }
-            }
-        }
-        return new List<string[]>() { p1Colors, p2Colors };
-    }
-
-    private List<string[]> DetermineManyPlayerColors () {
-        var words = new List<string[]>();
-        var colores = new List<string[]>();
-        foreach (var p in players) {
-            words.Add((string[]) p.Words.Clone());
-            colores.Add(GetInitialColors(wordsPerLine));
-        }
-
-        for (int i = 0; i < wordsPerLine; i++) {
-            var word = words[0][i];
-            int j = 1;
-            for (; j < players.Count; j++) {
-                if(!word.Equals(words[j][i])) {
-                    break;
-                }
-            }
-            if (j < players.Count)
-                continue;
-            for (int k = 0; k < players.Count; k++) {
-                colores[k][i] = "green";
-                words[k][i] = null;
-            }
-        }
-        var wordDict = new Dictionary<string, uint>();
-        foreach (var line in words) {
-            foreach (var word in line) {
-                if (word == null)
-                    continue;
-                else if (wordDict.ContainsKey(word))
-                    wordDict[word] += 1;
-                else
-                    wordDict[word] = 1;
-            }
-        }
-
-        foreach (var key in wordDict.Keys.ToList()) {
-            if (wordDict[key] < players.Count)
-                wordDict.Remove(key);
-        }
-
-        foreach (var key in wordDict.Keys.ToList()) {
-            for (int i = 0; i < words.Count; i++) {
-                for (int j = 0; j < words[i].Length; j++) {
-                    if(key.Equals(words[i][j])) {
-                        colores[i][j] = "yellow";
-                        wordDict[key] -= 1;
-                        if(wordDict[key] == 0) {
-                            goto Foo;
-                        }
-                    }
-                }
-            }
-            Foo:; // Wow
-        }
-        return colores;
-    }
-
-    private static string[] GetInitialColors (int length) {
-        return Enumerable.Repeat("red", length).ToArray();
-    }
-
-    private int ComputeScore (string[] colors) {
-        int score = 0;
-        foreach (var c in colors) {
-            if (c == "green")
-                score += 2;
-            else if (c == "yellow")
-                score += 1;
-        }
-        if (PerfectScore(wordsPerLine, score))
-            score += perfectScoreBonus;
-        return score;
-    }
-
     private void AddWordsToLineLogs (List<string[]> colors) {
         for (int i = 0; i < players.Count; i++) {
             lineLogs[i].AddLine(players[i].Words, colors[i]);
@@ -219,19 +94,20 @@ public class GameManager : NetworkBehaviour {
 
     private void ShowGameOverScreen () {
         var canvas = GameObject.Find("Canvas");
-        var go = Instantiate(gameOverPrefab, canvas.transform);
+        var go = Instantiate((GameObject) Resources.Load("GameOverScreen"), canvas.transform);
+        var gos = go.GetComponent<GameOverScreen>();
 
         switch (players.Count) {
             case 1:
-                go.AddData(lineLogs[0].PlayerName, " ",
-                    lineLogs[0].GetLinesAsString(), " ", lineScores.ToArray());
+                gos.AddData(lineLogs[0].PlayerName, " ",
+                    lineLogs[0].GetLinesAsString(), " ", colorWordMapper.GetScores());
                 break;
             case 2:
             case 3:
             case 4:
-                go.AddData(lineLogs[0].PlayerName, lineLogs[1].PlayerName,
+                gos.AddData(lineLogs[0].PlayerName, lineLogs[1].PlayerName,
                     lineLogs[0].GetLinesAsString(), lineLogs[1].GetLinesAsString(),
-                    lineScores.ToArray());
+                    colorWordMapper.GetScores());
                 break;
             default:
                 throw new ArgumentException();
@@ -261,12 +137,7 @@ public class GameManager : NetworkBehaviour {
         }
         return true;
     }
-
-    // True if score is such that 2 points per word was awarded
-    private static bool PerfectScore (int wordCount, int score) {
-        return score == wordCount * 2;
-    }
-
+    
     private bool IsGameFull () {
         return players.Count == MyNetworkManager.ExpectedPlayerCount;
     }
