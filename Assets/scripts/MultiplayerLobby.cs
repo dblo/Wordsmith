a22c35e21ed37259d1f71c0cac5a2c915142632f
Olle Olsson <ohjolsson@gmail.com>
2018-01-30 02:36:53 +0100

    Improving UI for mobile, lobby changes

---
+++ Assets/scripts/MultiplayerLobby.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MultiplayerLobby : MonoBehaviour {
+    private Slider gameLengthSlider;
+    private Slider playercountSlider;
+    private Slider seaModeSlider;
+    private Slider seaSizeSlider;
+    private Slider lineLengthSlider;
+    private Text gameLengthLabel;
+    private Text playerCountLabel;
+    private Text seaModeLabel;
+    private Text seaSizeLabel;
+    private Text lineLengthLabel;
+
+    void Start () {
+        gameLengthSlider = GameObject.Find("GameLengthSlider").GetComponent<Slider>();
+        playercountSlider = GameObject.Find("PlayerCountSlider").GetComponent<Slider>();
+        seaModeSlider = GameObject.Find("SeaModeSlider").GetComponent<Slider>();
+        seaSizeSlider = GameObject.Find("SeaSizeSlider").GetComponent<Slider>();
+        lineLengthSlider = GameObject.Find("LineLengthSlider").GetComponent<Slider>();
+        gameLengthLabel = GameObject.Find("GameLengthLabel").GetComponent<Text>();
+        playerCountLabel = GameObject.Find("PlayerCountLabel").GetComponent<Text>();
+        seaModeLabel = GameObject.Find("SeaModeLabel").GetComponent<Text>();
+        seaSizeLabel = GameObject.Find("SeaSizeLabel").GetComponent<Text>();
+        lineLengthLabel = GameObject.Find("LineLengthLabel").GetComponent<Text>();
+
+        gameLengthSlider.onValueChanged.AddListener(
+            (value) => gameLengthLabel.text = "Game length: " + value);
+
+        playercountSlider.onValueChanged.AddListener(
+             (value) => playerCountLabel.text = "Players: " + value);
+
+        seaModeSlider.onValueChanged.AddListener(
+             (value) => seaModeLabel.text = "Sea mode: " + value);
+
+        seaSizeSlider.onValueChanged.AddListener(
+             (value) => seaSizeLabel.text = "Sea size: " + value);
+
+        lineLengthSlider.onValueChanged.AddListener(
+             (value) => lineLengthLabel.text = "Line length: " + value);
+
+        var defaultPlayerCount = PlayerPrefs.GetInt("DefaultPlayerCount", -1);
+        if (defaultPlayerCount >= 0) {
+            if (playercountSlider.value == defaultPlayerCount) {
+                playerCountLabel.text = "Players: " + defaultPlayerCount;
+            }
+            playercountSlider.value = defaultPlayerCount;
+        } else {
+            playerCountLabel.text = "Players: " + playercountSlider.value;
+        }
+
+        var defaultGameLength = PlayerPrefs.GetInt("DefaultGameLength", -1);
+        if (defaultGameLength >= 0) {
+            gameLengthSlider.value = defaultGameLength;
+        }
+    }
+
+    public void StartGame () {
+        PlayerPrefs.SetInt("DefaultPlayerCount", (int) playercountSlider.value);
+        GameManager.ExpectedPlayerCount = (int) playercountSlider.value;
+
+        PlayerPrefs.SetInt("DefaultGameLength", (int) gameLengthSlider.value);
+        GameManager.LinesPerGame = (int) gameLengthSlider.value;
+
+        var nm = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        nm.StartHost();
+    }
+}
