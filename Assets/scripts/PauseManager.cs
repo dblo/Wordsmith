794243608daf18d2f7dde0b91ea192a4bcfb9ea1
Olle Olsson <ohjolsson@gmail.com>
2018-02-02 12:45:11 +0100

    Changed UI scaling, mobile friendleness, changed words in default lib

--- Assets/scripts/PauseManager.cs
+++ Assets/scripts/PauseManager.cs
@@ -6,14 +6,9 @@ public class PauseManager : MonoBehaviour {
     private GameObject pauseMenu;
 
     public void ToggleShowPauseMenu () {
-        var gm = GameObject.Find("GameManager");
         if (GamePaused()) {
             Destroy(pauseMenu);
-            if (gm != null)
-                gm.GetComponent<GameManager>().SetUIButtonsInteractable(true);
         } else {
-            if (gm != null)
-                gm.GetComponent<GameManager>().SetUIButtonsInteractable(false);
             GameObject canvas = GameObject.Find("Canvas");
             pauseMenu = Instantiate(pauseMenuPrefab, canvas.transform);
         }
--- Assets/scripts/WordSea.cs
+++ Assets/scripts/WordSea.cs
@@ -13,16 +13,11 @@ public class WordSea : MonoBehaviour {
 
     private List<Button> buttons;
     private List<string> currentSea;
-    string[] library = new string[]{ "copper","explain", "ill-fated", "truck", "neat",
-                                         "unite", "branch", "educated", "tenuous",
-                                         "hum", "decisive", "notice", "yell",
-                                         "parched", "attend", "punish", "push",
-                                         "pat", "zephyr", "retire", "anxious",
-                                         "trade", "suspend", "train", "brash",
-                                         "insurance", "groan", "remarkable", "print",
-                                         "cow", "wrench", "tremble", "surround",
-                                         "same", "double", "road", "is", "are", "you",
-                                         "what", "when", "like", "a", "your" };
+
+    string[] nouns = { "time","person","year","way","day","thing","man","world","life","hand","part","child","eye","woman","place","work","week","case","point","company","number" };
+    string[] verbs = { "be","have","do","say","get","make","go","know","take","see","come","think","look","want","give","use","find","tell","ask","work","seem","feel","try","leave","call" };
+    string[] adjectives = { "good", "first", "new", "last", "long", "great", "little", "own", "other", "old", "right", "big", "high", "different", "small", "large", "next", "early", "young", "important", "few", "public", "bad", "same", "able" };
+    string[] prepositions = { "to", "of", "in", "for", "on", "with", "at", "by", "from", "up", "about", "into", "over", "after" };
 
     private void Awake () {
         buttons = new List<Button>(wordSeaSize);
@@ -74,6 +69,15 @@ public class WordSea : MonoBehaviour {
 
     public string[] GenerateNewSea () {
         if (currentLibraryName == "Default") {
+            var library = new string[nouns.Length + verbs.Length + adjectives.Length + prepositions.Length];
+            Array.Copy(nouns, library, nouns.Length);
+            int ix = nouns.Length;
+            Array.Copy(verbs, 0, library, ix, verbs.Length);
+            ix += verbs.Length;
+            Array.Copy(adjectives, 0, library, ix, adjectives.Length);
+            ix += adjectives.Length;
+            Array.Copy(prepositions, 0, library, ix, prepositions.Length);
+
             var newSea = GenerateUniqueWords(wordSeaSize , library);
             return newSea;
         }
